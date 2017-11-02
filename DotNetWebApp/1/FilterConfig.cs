98049f597ba52abb600c4eade08f114adc2c4a6f
Reethika Ramesh <rerames@microsoft.com>
2017-11-02 17:49:16 +0530

    Add project files.

---
+++ DotNetWebApp/1/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace TaskWebApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DotNetWebApp/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskWebApp.Controllers
+{
+    public class ErrorController : System.Web.Mvc.Controller
+    {
+        // GET: Error
+        public System.Web.Mvc.ActionResult Index(string message)
+        {
+            ViewBag.Message = message;
+            return View("Error");
+        }
+    }
+}
